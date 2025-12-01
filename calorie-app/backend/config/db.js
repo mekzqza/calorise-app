be9669ebb7ee5d8304ca94571e517d764acaaa7e
sukpat matsombat <facebook007me@gmail.com>
2025-12-01 15:17:54 +0000

    เพิ่ม docker file ให้calories-backend

--- calorie-app/backend/config/db.js
+++ calorie-app/backend/config/db.js
@@ -3,12 +3,13 @@ import mongoose from 'mongoose';
 const DEFAULT_URI = process.env.MONGO_URI ?? 'mongodb://127.0.0.1:27017/calorie-app';
 
 export const connectDB = async () => {
-  try {
+  console.log('Connecting to MongoDB...');
+ /*  try {
     await mongoose.connect(DEFAULT_URI, {
       serverSelectionTimeoutMS: 5000,
     });
     console.log('MongoDB connected');
   } catch (error) {
     console.error('Mongo connection error', error.message);
-  }
+  } */
 };
